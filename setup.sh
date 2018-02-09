ff2aaa8ff35db0d9eb8ca659f885e0bb44ca3c74
Marco <estebanm297@gmail.com>
2018-02-08 19:57:44 -0500

    Updated setup to only add to faces_subset,txt when none exists

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-curl http://www.teach.cs.toronto.edu/~csc411h/winter/projects/proj1/facescrub_actors.txt >> faces_subset.txt
-curl http://www.teach.cs.toronto.edu/~csc411h/winter/projects/proj1/facescrub_actresses.txt >> faces_subset.txt
+
+if [! -f faces_subset.txt ]; then
+	curl http://www.teach.cs.toronto.edu/~csc411h/winter/projects/proj1/facescrub_actors.txt >> faces_subset.txt
+	curl http://www.teach.cs.toronto.edu/~csc411h/winter/projects/proj1/facescrub_actresses.txt >> faces_subset.txt
+fi
+
+if [ ! -f subset_actors.txt ]; then
+    touch subset_actors.txt
+fi
